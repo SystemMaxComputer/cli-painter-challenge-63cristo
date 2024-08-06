2f07ab1791e5c3c07e3b5719a78b016f65813044
Cristopher <geronimosanchezromero@gmail.com>
2024-08-06 11:44:35 -0500

    class Circle

--- painter/model.py
+++ painter/model.py
@@ -1,6 +1,28 @@
 # TODO: Add code here
+import math
+import matplotlib.pyplot as plt
 
-class point:
+
+class Point:
     def __int__(self, x: float, y: float):
         self.x: float = x
         self.y: float = y
+
+
+class Circle:
+    def __init__(self, center: Point, radius: float):
+        self.center: Point = center
+        self.radius: float = radius
+
+    def area(self) -> float:
+        area = math.pi * (self.radius ** 2)
+        return area
+
+    def draw(self):
+        circle = plt.Circle((self.center.x, self.center.y), self.radius, color="r")
+        plt.gca().add_patch(circle)
+        plt.axis("scaled")
+        plt.show()
+
+    def __str__(self):
+        return f"Circle with center at {self.center.x, self.center.y} and radius r: {self.radius}"
